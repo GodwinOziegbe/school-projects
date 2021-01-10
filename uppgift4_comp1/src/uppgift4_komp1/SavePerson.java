77a864e2144bfd7fe4cc2c94a3f1b62e4e02e884
Godwin Oziegbe <dwinonline@gmail.com>
2021-01-10 23:58:51 +0100

    uppgift4_komp1

---
+++ uppgift4_comp1/src/uppgift4_komp1/SavePerson.java
@@ -0,0 +1,34 @@
+package uppgift4_komp1;
+
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+
+
+public class SavePerson {
+	private static final String Person = "Person1.xml";
+	
+	public void savePersonData(ArrayList<Person> personList) {
+		XMLEncoder encoder = null;
+
+		try {
+
+			encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(Person)));
+			System.out.println("File stream opened and XMLEncoder created");
+
+		} catch (FileNotFoundException fileNotFound) {
+			System.out.println("ERROR: While Creating or Opening the File Person.xml");
+		}
+
+		encoder.writeObject(personList);
+		System.out.println("Object written");
+		encoder.flush();
+		encoder.close();
+		System.out.println("File stream closed");
+	}	
+	
+	
+}
